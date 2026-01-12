d3bb00d990e6af7646290927a169222c4e77b812
Dafeta <ejedafetaemmanuella@gmail.com>
2026-01-12 11:22:45 +0100

    fix: desktop nav bar

---
+++ ejes-haven-v2/script.js
@@ -0,0 +1,11 @@
+const navbar = document.getElementById("nav");
+
+if (navbar) {
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+}
